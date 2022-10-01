233be34caef52a3197051d3430c594d92375b95f
javivilladamigo <javier.marinovilladamigo@studenti.unipd.it>
2022-10-01 11:53:50 +0200

    deleted skimTest.py and test_read

--- test_read.py
+++
@@ -1,5 +0,0 @@
-txt_file = "fileList_v9/GluGluToH_HToJPsiG_JPsiToMuMu_TuneCP5_13TeV-madgraph-pythia8_RunIISummer20UL18.txt"
-fileList = []
-with open(txt_file) as file:
-    for line in file:
-        fileList.append("root://cms-xrd-global.cern.ch/" + line.rstrip("\n"))
