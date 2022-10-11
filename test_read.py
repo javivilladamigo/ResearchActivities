f890ccf5acaf1b3000fa449b498b87372bd22345
javivilladamigo <javier.marinovilladamigo@studenti.unipd.it>
2022-10-11 19:38:25 +0200

    added test_read_remote() and test_read_local() functions

--- test_read.py
+++ test_read.py
@@ -1,5 +1,14 @@
-txt_file = "fileList_v9/GluGluToH_HToJPsiG_JPsiToMuMu_TuneCP5_13TeV-madgraph-pythia8_RunIISummer20UL18.txt"
-fileList = []
-with open(txt_file) as file:
-    for line in file:
-        fileList.append("root://cms-xrd-global.cern.ch/" + line.rstrip("\n"))
+from os import listdir
+from os.path import isfile, join
+
+def test_read_remote(txt_file = "fileList_v9/GluGluToH_HToJPsiG_JPsiToMuMu_TuneCP5_13TeV-madgraph-pythia8_RunIISummer20UL18.txt"):
+    fileList = []
+    with open(txt_file) as file:
+        for line in file:
+            fileList.append("root://cms-xrd-global.cern.ch/" + line.rstrip("\n"))
+    return fileList
+
+def test_read_local(dir_file = "/Users/javi/Documents/Padova/Research.nosync/ResearchActivities/data.nosync/OutputTest/"):
+    fileList = [f for f in listdir(dir_file) if (isfile(join(dir_file, f)) and ".root" in f)]
+    return fileList
+
